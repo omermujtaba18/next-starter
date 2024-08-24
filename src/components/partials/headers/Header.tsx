a4a63c288b2d26ff3c67712f2ad6fe9b8b674d19
Omer Mujtaba <omer.mujtaba96@gmail.com>
2024-08-24 06:21:17 +0300

    feat(components:shared): add hideNav support to header

--- src/components/partials/headers/Header.tsx
+++ src/components/partials/headers/Header.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default () => {
+export default ({ hideNav = false }: { hideNav?: boolean }) => {
   return (
     <header className="bg-slate-50">
       <div className="container mx-auto flex min-h-16 flex-row items-center justify-between px-6 align-middle md:px-0">
         <Link href="/">
           <Image src="/next.svg" alt="logo" width="100" height="100" />
         </Link>
+        {!hideNav && (
+          <nav className="flex gap-8">
+            <Link href="/design">Design System</Link>
+            <Link href="/login">Login</Link>
+            <Link href="/register">Register</Link>
+          </nav>
+        )}
       </div>
     </header>
   );
